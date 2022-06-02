6d7c97adf05572502b0d6c10ffa913169b7409a1
delrio <delriodeveloper@gmail.com>
2022-06-02 15:24:51 +0200

    styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaInstagram,FaTiktok,FaYoutube,FaTwitter,FaTwitch } from "react-icons/fa";
 import LiCon from './LiCon';
 
+import './styles/Footer.css'
 
 const Footer = () => {
   return (
@@ -15,10 +16,23 @@ const Footer = () => {
             <LiCon goto='https://youtube.com/channel/UC2Rg4nOELkGCojl0eIXPp9Q'><FaYoutube/> YouTube</LiCon>
             <LiCon goto='https://twitch.tv/itslachowski'><FaTwitch/> Twitch</LiCon>
             <LiCon goto='https://twitter.com/arepaconhuevo_'><FaTwitter/> Twitter</LiCon>
-
           </ul>
         </div>
 
+
+        <div className='footer-links'>
+          <p>Tambien te puede interesar</p>
+          <ul>
+            <LiCon goto='/cursos'>Cursos</LiCon>
+            <LiCon goto='/'>Home</LiCon>
+            <LiCon goto='/blog'>Blog</LiCon>
+            <LiCon goto='/colaboraciones'>Colaboraciones</LiCon>
+          </ul>
+        </div>
+
+        <div className='footer-copyright'>
+          <p>© 2022-2022 itslachoski. Todos los derechos reservados</p>
+        </div>
     </footer>
   )
 }
